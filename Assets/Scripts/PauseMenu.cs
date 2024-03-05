00c42475b1a0a825a65e64f9af5e71768bfdca79
GenetixxUTD <cameronshirley8@googlemail.com>
2024-03-05 07:41:41 +0000

    Settings Menu Logic Added
    EXCEPT FROM
    - Volume Slider (Wwise to be integrated)
    - Save Game (Save Game Logic not yet created)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,11 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public charactercontroller PlayerCharacter;
+
+    public GameObject SettingsTabGroup;
+    public GameObject InventoryTabGroup;
+
+    public GameObject SettingsButton;
+    public GameObject InventoryButton;
+
+    public TMP_Dropdown ResolutionDropdown;
+
+    private Resolution[] Resolutions;
+
+    private void Start()
+    {
+        Resolutions = Screen.resolutions;
+        ResolutionDropdown.ClearOptions();
+
+        List<string> optionsList = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < Resolutions.Length; i++)
+        {
+            string option = Resolutions[i].width + " x " + Resolutions[i].height;
+            optionsList.Add(option);
+
+            if (Resolutions[i].width == Screen.currentResolution.width && Resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        ResolutionDropdown.AddOptions(optionsList);
+        ResolutionDropdown.value = currentResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
+    }
 
     private void Update()
     {
@@ -21,5 +58,47 @@ public class PauseMenu : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         this.gameObject.SetActive(false);
+    }
+
+    public void SetVolume(float volume)
+    {
+        //Wwise integration here
+    }
+
+    public void SetGraphicsQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void UpdateResolution(int resolutionIndex)
+    {
+        Resolution resolutionToBe = Resolutions[resolutionIndex];
+        Screen.SetResolution(resolutionToBe.width, resolutionToBe.height, Screen.fullScreen);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SettingsTab()
+    {
+        SettingsTabGroup.SetActive(true);
+        SettingsButton.SetActive(false);
+        InventoryTabGroup.SetActive(false);
+        InventoryButton.SetActive(true);
+    }
+
+    public void InventoryTab()
+    {
+        SettingsTabGroup.SetActive(false);
+        SettingsButton.SetActive(true);
+        InventoryTabGroup.SetActive(true);
+        InventoryButton.SetActive(false);
     }
 }
