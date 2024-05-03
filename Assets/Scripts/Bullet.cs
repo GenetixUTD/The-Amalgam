b6bc3d976f28732a9b923d732403351afaf5eec7
GenetixxUTD <cameronshirley8@googlemail.com>
2024-05-03 06:03:50 +0100

    Reverted bullet debug changes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private float despawnTime = 5;
-    private float MovementSpeed = 5;
+    private float MovementSpeed = 50;
 
     // Start is called before the first frame update
     void Start()
